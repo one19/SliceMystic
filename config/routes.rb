782122ebdf37b12be3af8d4ebd4e3bcdc433a8aa
Drew Showalter <maelstroswim@gmail.com>
2015-05-30 08:57:36 +1000

    ran controller create for pages and users

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@
 #
 
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'pages/home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
